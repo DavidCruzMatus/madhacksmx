7307f1900b4a1d3651b68e27bd0e73372a6b1b61
DavidCruzMatus <70873013+DavidCruzMatus@users.noreply.github.com>
2020-11-29 18:28:55 -0500

    Add files via upload

--- mlenvironment_01.sh
+++ mlenvironment_01.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Create the environment:
-wget --quiet https://repo.anaconda.com/miniconda/Miniconda2-4.5.11-Linux-x86_64.sh -O ~/miniconda.sh && \
+wget https://repo.anaconda.com/miniconda/Miniconda2-4.5.11-Linux-x86_64.sh -O ~/miniconda.sh && \
     /bin/bash ~/miniconda.sh -b -p /opt/conda && \
     rm ~/miniconda.sh && \
     ln -s /opt/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh && \
@@ -8,4 +8,3 @@ wget --quiet https://repo.anaconda.com/miniconda/Miniconda2-4.5.11-Linux-x86_64.
 echo "export PATH=$PATH:/opt/conda/bin" >> ~/.bashrc
 conda update -y -n base -c defaults conda
 conda create -y --name madhacksmx -y python=3.7 tensorflow numpy PyYAML pip flask flask-socketio
-conda init bash
