9a4dc49d3def5a05fb034f2741fced80f01250e6
DavidCruzMatus <70873013+DavidCruzMatus@users.noreply.github.com>
2020-11-29 15:03:39 -0600

    Add files via upload

--- mlenvironment_01.sh
+++ mlenvironment_01.sh
@@ -7,5 +7,5 @@ wget --quiet https://repo.anaconda.com/miniconda/Miniconda2-4.5.11-Linux-x86_64.
     echo ". /opt/conda/etc/profile.d/conda.sh" >> ~/.bashrc 
 echo "export PATH=$PATH:/opt/conda/bin" >> ~/.bashrc
 conda update -y -n base -c defaults conda
-conda create -y --name madhacksmx -y python=3.7 tensorflow numpy PyYAML pip flask
+conda create -y --name madhacksmx -y python=3.7 tensorflow numpy PyYAML pip flask flask-socketio
 conda init bash
