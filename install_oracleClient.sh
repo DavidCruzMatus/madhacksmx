57137e96af62a820a592cfc59018107ee341f60e
DavidCruzMatus <70873013+DavidCruzMatus@users.noreply.github.com>
2020-11-29 19:03:09 -0500

    Add files via upload

--- install_oracleClient.sh
+++ install_oracleClient.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get install -y zip
-unzip /root/oracle/instantclient-basic-linux.x64-19.6.0.0.0dbru.zip
+unzip /root/oracle/instantclient-basic-linux.x64-19.9.0.0.0dbru.zip
 echo "export PATH=$PATH:/root/oracle/instantclient_19_9" >> ~/.bashrc
 echo "export TNS_ADMIN=/root/oracle/instantclient_19_9/network/admin" >> ~/.bashrc
 echo "export LD_LIBRARY_PATH=/root/oracle/instantclient_19_9" >> ~/.bashrc
